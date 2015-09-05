357d572432b0dd6513c6e973f6632efb96e16464
Miguel Castillo <manchagnu@gmail.com>
2015-09-05 01:25:54 -0400

    Removed unit tests as they are boilerplate ones from YO

--- test/spec/index.js
+++ test/spec/index.js
@@ -1,13 +1,8 @@
 import index from 'dist/index';
 
 describe('Test suite', () => {
-  var pandaBear;
   beforeEach(() => {
-    pandaBear = new index();
-    sinon.stub(pandaBear, 'eats').returns('Bamboo and more');
   });
 
-  it('pandaBear.eats returns `Bamboo and more`', () => {
-    expect(pandaBear.eats()).to.equal('Bamboo and more');
-  });
+  it('pandaBear.eats returns `Bamboo and more`', false);
 });
