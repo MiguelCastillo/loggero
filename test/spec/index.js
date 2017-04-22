a0d87fa17da1e7d50dfb9ee419d1395217a35c5f
Miguel Castillo <mcastillo@symphono.com>
2017-04-21 23:32:15 -0400

    removed unused tests

--- test/spec/index.js
+++
@@ -1,9 +0,0 @@
-import logger from 'dist/index';
-
-describe('Test suite', () => {
-  beforeEach(() => {
-    logger.log('Logger is called');
-  });
-
-  it('pandaBear.eats returns `Bamboo and more`', false);
-});
